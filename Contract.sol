e270b83ef94b7634a2747647adb7407f700211c2
Aditya.Hadap <106440327+LifelessPROgrammer@users.noreply.github.com>
2024-08-15 22:49:18 +0530

    Create Contract.sol

---
+++ Contract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Contract_demo {
+string message = "Hello";
+function dispMsg() public view returns (string memory) {
+return message;
+}
+}
