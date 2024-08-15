677b3cf76eade967522ccda155a625a83fbbb868
Aditya.Hadap <106440327+LifelessPROgrammer@users.noreply.github.com>
2024-08-15 11:20:16 +0530

    Create WhileLoop.sol

---
+++ WhileLoop.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;contract
+whiledemo
+{
+uint [] data;uint
+x=0;
+
+function whileLoopDemo() public
+{
+while(x<5)
+{
+data.push(x);
+x=x+1;
+}
+}
+
+function dispwhileloop() public view returns(uint[] memory)
+{
+return data;
+}
+}
